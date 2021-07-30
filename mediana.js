237e070e5a178e8b06eedf44098a768505265e3f
Carlos Corrales <carlox59681@gmail.com>
2021-07-29 19:10:32 -0500

    calcular la mediana a traves de una lista que de el usuario

---
+++ mediana.js
@@ -0,0 +1,48 @@
+function calcularMediaAritmetica(lista)
+{ 
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento)
+        {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
+}
+
+function calcularMediana(lista)
+{
+    const listaOrdenada = lista.sort(function(primerElemento, segundoElemento)
+        {
+            return primerElemento - segundoElemento
+        });
+
+    const mitadLista = parseInt(listaOrdenada.length / 2);
+
+    function esPar(numero)
+    {
+        if(numero % 2 === 0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    if(esPar(lista.length))
+    {
+        const elemento1 = listaOrdenada[mitadLista - 1];
+        const elemento2 = listaOrdenada[mitadLista];
+
+        const promedioElemento1y2 = calcularMediaAritmetica([elemento1,elemento2]);
+    
+        return promedioElemento1y2;
+    }   
+    else
+    {
+        return listaOrdenada[mitadLista];
+    }
+}
